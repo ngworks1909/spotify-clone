e44352843c7ad9ec80d7e10d21fabd76b706d320
Nithin1908 <76480955+ngworks1909@users.noreply.github.com>
2024-03-09 00:55:06 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -4,7 +4,12 @@ const cors = require("cors");
 const app = express();
 const port = 3001;
 
-app.use(cors());
+app.use(
+    cors({
+      origin: ["http://localhost:3000", "https://ngworks-spotify.vercel.app"],
+      credentials: true,
+    })
+);
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
